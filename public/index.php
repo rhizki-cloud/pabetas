354d8524aaafdd4165ab6de81c1d90dc919e884c
Rhizki <ahmadrhizki234@gmail.com>
2026-06-30 10:25:58 +0700

    redirect root to login

--- public/index.php
+++ public/index.php
@@ -1,6 +1,10 @@
 <?php
 require_once __DIR__ . '/../app/init.php';
-if (current_user()) {
-    redirect($_SESSION['role'] === 'guru' ? 'teacher/dashboard.php' : 'student/dashboard.php');
+
+$user = current_user();
+
+if ($user) {
+    redirect($user['role'] === 'guru' ? 'teacher/dashboard.php' : 'student/dashboard.php');
 }
+
 redirect('login.php');
